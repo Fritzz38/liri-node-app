2f08d04e41b795df1084618846dc77f83ae479c7
Tarmin Sidharta <tsidharta@gmail.com>
2017-06-26 00:34:41 -0700

    completed

--- liri.js
+++ liri.js
@@ -35,11 +35,11 @@ function myTweets() {
   consumer_secret: keys.twitterKeys.consumer_secret,
   access_token_key: keys.twitterKeys.access_token_key,
   access_token_secret: keys.twitterKeys.access_token_secret
-});
+  });
 
-var params = { screen_name: 'tsidhar', count: 2};
+  var params = { screen_name: 'tsidhar', count: 2};
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  client.get('statuses/user_timeline', params, function(error, tweets, response) {
   if (!error) {
 
   	for (i = 0; i < tweets.length; i++) {    
@@ -49,7 +49,6 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
     }
   }
 });
-
 
 }
 
@@ -90,18 +89,17 @@ spotify.search({ type: 'track', query: song }, function(err, data) {
     return console.log('Error occurred: ' + err);
   }
     var results = data.tracks.items[0];
- 	console.log("Song: " + results.name); 	
- 	console.log("Artist: " + results.artists[0].name);
- 	console.log("Album: " + results.album.name);
- 	console.log("Preview Link: " + results.preview_url);
+ 	  console.log("Song: " + results.name); 	
+ 	  console.log("Artist: " + results.artists[0].name);
+ 	  console.log("Album: " + results.album.name);
+ 	  console.log("Preview Link: " + results.preview_url);
 });
-
 
 }
  
 
 
-function movieThis () {
+function movieThis(movie) {
 
 var movie = process.argv[3];
 
@@ -124,34 +122,60 @@ request(searchURL, function (error, response, body) {
   
 });
 
-
 }
 
 
 
 function doWhatItSays() {
 
- fs.readFile("random.txt", "utf8", function(err, data) {
- 	var res = data.split(",");
-  
-  	var params = res[1];
-  	var spotify = new Spotify({
-        id: 'cb6b14492dda4863b1ff63f13e9fe65c',
-        secret: '19aa280683414cedbb3ebc41bc447e1f'
-        });
+  fs.readFile("random.txt", "utf8", function(err, data) {
+   	var res = data.split(",");
+    
+    if (res[0] === "spotify-this-song") {
 
-  spotify.search({ type: 'track', query: params }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
-    var results = data.tracks.items[0];
- 	console.log("Song: " + results.name); 	
- 	console.log("Artist: " + results.artists[0].name);
- 	console.log("Album: " + results.album.name);
- 	console.log("Preview Link: " + results.preview_url);
-});
-  
+      var params = res[1];
 
- });
+      var spotify = new Spotify({
+           id: 'cb6b14492dda4863b1ff63f13e9fe65c',
+          secret: '19aa280683414cedbb3ebc41bc447e1f'
+          });
+
+      spotify.search({ type: 'track', query: params }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+          }
+
+          var results = data.tracks.items[0];
+          console.log("Song: " + results.name);   
+          console.log("Artist: " + results.artists[0].name);
+          console.log("Album: " + results.album.name);
+          console.log("Preview Link: " + results.preview_url);
+          });
+
+    }
+
+
+    if (res[0] === "movie-this") {
+
+      var params = res[1];
+
+      var searchURL = "http://www.omdbapi.com/?apikey=40e9cece&t=" + params + "&y=&plot=short&r=json";
+
+      request(searchURL, function (error, response, body) {
+
+      console.log("Title: " + JSON.parse(body).Title);
+      console.log("Year: " + JSON.parse(body).Year);
+      console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value); 
+      console.log("Country: " + JSON.parse(body).Country);
+      console.log("Language: " + JSON.parse(body).Language);
+      console.log("Plot: " + JSON.parse(body).Plot);
+      console.log("Actors: " + JSON.parse(body).Actors);
+    
+      });
+
+      }
+
+  });
 
 }
